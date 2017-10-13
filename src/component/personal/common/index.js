aab1fc8bc53c4b9ebe6030d641db4bbf0df2bb63
陈国荣 <1178310248@qq.com>
2017-10-13 11:44:01 +0800

    提交status组件

--- src/component/personal/common/index.js
+++ src/component/personal/common/index.js
@@ -2,7 +2,7 @@
  * 一些通用的共用组件
  **/
 import React, {PureComponent} from 'react';
-import {Flex, List} from 'antd-mobile';
+import {Flex, NoticeBar} from 'antd-mobile';
 import {
     View,
     StyleSheet,
@@ -18,41 +18,89 @@ import {
  */
 export const Item = (props) => {
     return(
-        <Flex style={ItemStyles.listItem}>
-            <Flex.Item style={ItemStyles.titleWrap}>
+        <View style={ItemStyles.listItem}>
+            <View style={ItemStyles.titleWrap}>
                 <Text style={ItemStyles.listTitle}>
                     {props.name?props.name:''}
                 </Text>
-            </Flex.Item>
-            <Flex.Item style={ItemStyles.listContent}>
+            </View>
+            <View style={ItemStyles.listContent}>
                 <Text style={ItemStyles.listText}>
-                    {props.text?props.text:''}
+                    {props.text && props.text != null?props.text:''}
                 </Text>
-            </Flex.Item>
-        </Flex>
+            </View>
+        </View>
     )
 }
 
 const ItemStyles = StyleSheet.create({
     listItem: {
-        height: 50,
+        display: 'flex',
+        flexDirection: 'row',
         paddingLeft: 10,
+        height: 50,
         borderBottomWidth: 1/PixelRatio.get(),
         borderStyle: 'solid',
         borderColor: '#dddddd',
     },
     titleWrap: {
-        flex: 2,
+        justifyContent: 'center',
     },
     listContent: {
-        flex: 3,
+        justifyContent: 'center',
     },
     listTitle: {
-        fontSize: 18,
+        fontSize: 15,
         marginRight: 10,
     },
     listText: {
-        fontSize: 16,
+        fontSize: 14,
         color: '#9F9F9F',
+    }
+})
+
+/**
+ * 顶部审批状态信息
+ * @param props
+ * @returns {XML}
+ * @constructor
+ */
+export const NoticeBarMessage = (props) => {
+    return(
+        <View>
+            {
+                props.status == 'N'?
+                    <NoticeBar>
+                        您的信息已提交成功，等待审批中。
+                    </NoticeBar>:
+                    props.status == 'P'?
+                        <NoticeBar>
+                            您的信息正在审批中。
+                        </NoticeBar>:
+                        props.status == 'R'?
+                            <NoticeBar>
+                                您的信息已审批不通过。
+                            </NoticeBar>:
+                            props.status == 'A'?
+                                <NoticeBar>
+                                    您的信息已审批通过。
+                                </NoticeBar>:
+                                null
+            }
+        </View>
+    )
+}
+
+export const RequireData = () => {
+    return(
+        <Text style={requireStyle.text}>
+            *
+        </Text>
+    )
+}
+
+const requireStyle = StyleSheet.create({
+    text: {
+        color: 'red',
     }
 })
