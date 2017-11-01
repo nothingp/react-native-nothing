da70c9f830834fb3dda197479dbdbab53c073287
jundy.chow <zhoujundi711@163.com>
2017-11-01 09:51:03 +0800

    [feat]修改自定义样式文件

--- scripts/custom-rn-theme.js
+++ scripts/custom-rn-theme.js
@@ -1,12 +1,16 @@
-
 const path = require('path');
 const fs = require('fs');
-
-const defaultVars = require('antd-mobile/lib/style/themes/default');
+// for 1.x
+// const defaultVars = require('antd-mobile/lib/style/themes/default');
+// for 2.x
+const defaultVars = require('antd-mobile/lib/style/themes/default.native');
 const customVars = require('../theme');
-const themePath = path.resolve(require.resolve('antd-mobile'), '../style/themes/default.js');
-
+// for 1.x
+// const themePath = path.resolve(require.resolve('antd-mobile'), '../style/themes/default.js');
+// for 2.x
+const themePath = path.resolve(require.resolve('antd-mobile'), '../style/themes/default.native.js');
 const themeVars = Object.assign({}, defaultVars, customVars);
+
 if (fs.statSync(themePath).isFile()) {
     fs.writeFileSync(
         themePath,
