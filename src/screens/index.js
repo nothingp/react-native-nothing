8ee63c1c0deaeeddaa3b3564f1c5986f0d0bafcf
jundy.chow <zhoujundi711@163.com>
2017-11-03 09:29:54 +0800

    [feat]修改app的display name

--- src/screens/index.js
+++ src/screens/index.js
@@ -141,27 +141,31 @@ export function startTabsScreen(isManager) {
         delete tabs.Tab2
     }
 
-    return TabNavigator(
-        tabs,
-        {
-            animationEnabled: false, // 切换页面时不显示动画
-            tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
-            swipeEnabled: false, // 禁止左右滑动
-            backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
-            lazy: true,
-            tabBarOptions: {
-                activeTintColor: gColors.brandPrimary, // 文字和图片选中颜色
-                inactiveTintColor: '#999', // 文字和图片默认颜色
-                showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
-                indicatorStyle: { height: 0 }, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了， 不知道还有没有其它方法隐藏？？？
-                style: {
-                    height: Platform.OS == 'android' ? 63 : 55,
-                    backgroundColor: '#fff', // TabBar 背景色
-                },
-                labelStyle: {
-                    fontSize: 12, // 文字大小
-                },
+    let config = {
+        animationEnabled: false, // 切换页面时不显示动画
+        tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
+        swipeEnabled: false, // 禁止左右滑动
+        backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
+        lazy: true,
+        tabBarOptions: {
+            activeTintColor: gColors.brandPrimary, // 文字和图片选中颜色
+            inactiveTintColor: '#999', // 文字和图片默认颜色
+            showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
+            indicatorStyle: { height: 0 }, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了， 不知道还有没有其它方法隐藏？？？
+            style: {
+                backgroundColor: '#fff', // TabBar 背景色
             },
-        });
+            labelStyle: {
+                fontSize: 12, // 文字大小
+            },
+        },
+    }
+
+    //设置tab的高度
+    if(Platform.OS == 'android'){
+        config.tabBarOptions.style.height = 63;
+    }
+
+    return TabNavigator(tabs,config);
 }
 
