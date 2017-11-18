41c299660863936777019fb233c6f992ce003cb8
truexinology <truexin@163.com>
2017-11-19 01:08:42 +0800

    [fix] 恢复add页面

--- src/component/daily/reimbursement/addClaims.js
+++ src/component/daily/reimbursement/addClaims.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+
+import React, {Component} from 'react';
 import {
     View,
     Text,
@@ -8,49 +9,37 @@ import {
     Image
 } from 'react-native';
 
-import { RequireData } from '../../personal/common/index';
-import {
-    List,
-    Picker,
-    DatePicker,
-    Flex,
-    InputItem,
-    Button,
-    Icon,
-    ActionSheet,
-    WhiteSpace,
-    WingBlank
-} from 'antd-mobile';
+import {RequireData} from '../../personal/common/index';
+import { List,Picker, DatePicker, Flex, InputItem, Button, Icon, ActionSheet, WhiteSpace, WingBlank} from 'antd-mobile';
 import { inject, observer } from 'mobx-react/native';
 import { createForm } from 'rc-form';
 import ImagePicker from 'react-native-image-picker';
 import Base from '../../../stores/Base'
-import { format } from '../../../common/Tool';
-
+import {format} from '../../../common/Tool';
 // import Flex from "antd-mobile/es/flex/Flex.d";
 
-@inject('User', 'Common', 'True')
+@inject('User', 'Common','True')
 @observer
-class Index extends Component {
+class Index extends Component{
     static navigationOptions = ({ navigation }) => {
         return {
-            title: '报销项增加',
+            title:'报销项增加',
         }
     };
     state = {
         typeValue: '',
     }
 
-    constructor(props) {
+    constructor(props){
         super(props);
         this.state = {
             imgInfo: '',
-            doctor_certificate: ''
+            doctor_certificate:''
         }
     }
 
     changeType = (v) => {
-        const { claimsType } = this.props.Common;
+        const {claimsType} = this.props.Common;
         console.log(v);
         console.log(claimsType[v].label)
         //1， 如果某个假期类型对应的alert_msg_desc字段不为空的话，选择该假期的时候需要在假期类型下面显示这个字段的内容；为空的话，则不需要显示。
@@ -62,7 +51,6 @@ class Index extends Component {
 
 
     }
-
     componentWillMount() {
         //请求假期类型数据
         this.props.Common.getClaimsType();
@@ -71,25 +59,25 @@ class Index extends Component {
 
     }
 
-    loadJob = (gl_type, i) => {
-        const { claimsJob } = this.props.Common
-        this.props.Common.getClaimsJob(gl_type, i);
+    loadJob = (gl_type,i)=>{
+        const {claimsJob} = this.props.Common
+        this.props.Common.getClaimsJob(gl_type,i);
     }
 
-    changeRightValue = (a, b) => {
+    changeRightValue = (a,b) => {
         // console.log((b+'').subString(a.length+3));
         // console.log(a)
         // console.log(b.replace(a+' - ',''));
-        return b.replace(a + ' - ', '');
+        return b.replace(a+' - ','');
     }
 
-    goShowList = (gl_type, gl_seg_label, i) => {
-        this.props.navigation.navigate('ShowList', { gl_type: gl_type, gl_seg_label: gl_seg_label, i: i })
+    goShowList = (gl_type,gl_seg_label, i)=> {
+        this.props.navigation.navigate('ShowList', {gl_type: gl_type, gl_seg_label:gl_seg_label, i:i})
     }
 
     onSubmit = () => {
         const { form } = this.props;
-        const { claimsType, claimsDetail, claimsJob, claimsDepartment, claimsGroup, claimsTeam, claimsPayment, currencyList } = this.props.Common;
+        const {claimsType, claimsDetail, claimsJob, claimsDepartment, claimsGroup, claimsTeam, claimsPayment, currencyList} = this.props.Common;
         console.log(this.state.imgInfo);
         const { imgInfo } = this.state;
 
@@ -127,19 +115,15 @@ class Index extends Component {
                 claimitemsv2.as_of_date = sdate;
                 claimitemsv2.amount = money;
 
-
                 //上传图片
-                if (imgInfo) {
-                    this.props.Common.imgUpload(imgInfo, () => {
-                    })
+                if(imgInfo){
+                    this.props.Common.imgUpload(imgInfo,()=>{})
                 }
-
-                alert(1)
 
                 // claimitems
                 // console.log(claimitemsv2)
                 this.props.True.addclaimsItemAction(claimitemsv2);
-                this.props.navigation.navigate('ClaimsDetail', { info: { status: 'create' } });
+                this.props.navigation.navigate('ClaimsDetail', { info: {status:'create'} });
             }
         })
     }
@@ -149,22 +133,22 @@ class Index extends Component {
             title: 'Select Avatar'
         };
 
-        return (
+        return(
             <List renderHeader={() => '附件'}>
                 <TouchableOpacity onPress={() => {
                     const BUTTONS = ['相册', '拍照', '取消'];
                     ActionSheet.showActionSheetWithOptions({
                         options: BUTTONS,
                         cancelButtonIndex: BUTTONS.length - 1
-                    }, (buttonIndex) => {
-                        if (buttonIndex == 0) {
-                            ImagePicker.launchImageLibrary(options, (response) => {
+                    },(buttonIndex) => {
+                        if(buttonIndex==0){
+                            ImagePicker.launchImageLibrary(options, (response)  => {
                                 this.setState({
                                     imgInfo: response
                                 })
                             });
-                        } else if (buttonIndex == 1) {
-                            ImagePicker.launchCamera(options, (response) => {
+                        }else if(buttonIndex==1){
+                            ImagePicker.launchCamera(options, (response)  => {
                                 this.setState({
                                     imgInfo: response
                                 })
@@ -174,9 +158,8 @@ class Index extends Component {
                     });
                 }}>
                     {
-                        imgInfo || doctor_certificate ?
-                            <Image style={styles.image}
-                                   source={{ uri: imgInfo.uri ? imgInfo.uri : doctor_certificate }}/> :
+                        imgInfo || doctor_certificate?
+                            <Image style={styles.image} source={{uri: imgInfo.uri ? imgInfo.uri:doctor_certificate}}/>:
                             <View style={styles.image}>
                                 <Text style={styles.text}>
                                     <Icon type={'\ue910'} size="xl" color="#D2D2D2"/>
@@ -190,17 +173,18 @@ class Index extends Component {
     }
 
 
+
     render() {
         const { getFieldProps } = this.props.form;
         const { imgInfo, doctor_certificate } = this.state;
 
-        const { claimsType, claimsDetail, claimsJob, claimsDepartment, claimsGroup, claimsTeam, claimsPayment, currencyList } = this.props.Common;
-        const { typeValue } = this.state;
+        const {claimsType, claimsDetail, claimsJob, claimsDepartment, claimsGroup, claimsTeam, claimsPayment, currencyList} = this.props.Common;
+        const {typeValue} = this.state;
         // console.log(claimsDetail)
         // console.log({...claimsDepartment});
 
-        return (
-            <View style={{ backgroundColor: '#fff' }}>
+        return(
+            <View  style={{backgroundColor:'#fff'}}>
                 <List>
                     <Picker data={claimsType} cols={1}
                             {
@@ -226,8 +210,8 @@ class Index extends Component {
                                     ...getFieldProps(
                                         'sdate',
                                         {
-                                            initialValue: '',
-                                            rules: [{ required: true }],
+                                            initialValue:'',
+                                            rules: [{required: true}],
 
                                         }
                                     )
@@ -283,70 +267,60 @@ class Index extends Component {
                     </Flex>
                 </List>
                 {
-                    claimsDetail.gl_seg1_label &&
+                    claimsDetail.gl_seg1_label&&
                     <List>
                         <List.Item
                             arrow="horizontal"
                             extra={claimsDepartment.label || claimsDetail.gl_seg1_default_desc}
-                            onClick={() => {
-                                this.goShowList(claimsDetail.gl_seg1_type, claimsDetail.gl_seg1_label, 1)
-                            }}
+                            onClick={()=>{ this.goShowList(claimsDetail.gl_seg1_type,claimsDetail.gl_seg1_label, 1) }}
                         >
                             {claimsDetail.gl_seg1_label}
                         </List.Item>
                     </List>
                 }
                 {
-                    claimsDetail.gl_seg2_label &&
+                    claimsDetail.gl_seg2_label&&
                     <List>
                         <List.Item
                             arrow="horizontal"
                             extra={claimsGroup.label || claimsDetail.gl_seg1_default_desc}
-                            onClick={() => {
-                                this.goShowList(claimsDetail.gl_seg2_type, claimsDetail.gl_seg2_label, 2)
-                            }}
+                            onClick={()=>{ this.goShowList(claimsDetail.gl_seg2_type,claimsDetail.gl_seg2_label, 2) }}
                         >
                             {claimsDetail.gl_seg2_label}
                         </List.Item>
                     </List>
                 }
                 {
-                    claimsDetail.gl_seg3_label &&
+                    claimsDetail.gl_seg3_label&&
                     <List>
                         <List.Item
                             arrow="horizontal"
                             extra={claimsTeam.label || claimsDetail.gl_seg3_default_desc}
-                            onClick={() => {
-                                this.goShowList(claimsDetail.gl_seg3_type, claimsDetail.gl_seg3_label, 3)
-                            }}
+                            onClick={()=>{ this.goShowList(claimsDetail.gl_seg3_type,claimsDetail.gl_seg3_label ,3) }}
                         >
                             {claimsDetail.gl_seg3_label}
                         </List.Item>
                     </List>
                 }
                 {
-                    claimsDetail.gl_seg4_label &&
+                    claimsDetail.gl_seg4_label&&
                     <List>
                         <List.Item
                             arrow="horizontal"
                             extra={claimsJob.label || claimsDetail.gl_seg4_default_desc}
-                            onClick={() => {
-                                this.goShowList(claimsDetail.gl_seg4_type, claimsDetail.gl_seg4_label, 4)
-                            }}
+                            onClick={()=>{ this.goShowList(claimsDetail.gl_seg4_type,claimsDetail.gl_seg4_label, 4) }}
                         >
                             {claimsDetail.gl_seg4_label}
                         </List.Item>
                     </List>
                 }
                 {
-                    claimsDetail.gl_seg5_label &&
+                    claimsDetail.gl_seg5_label&&
                     <List>
                         <List.Item
                             arrow="horizontal"
                             extra={claimsPayment.label || claimsDetail.gl_seg5_default_desc}
-                            onClick={() => {
-                                this.goShowList(claimsDetail.gl_seg5_type, claimsDetail.gl_seg5_label, 5)
-                            }}
+                            onClick={()=>{ this.goShowList(claimsDetail.gl_seg5_type,claimsDetail.gl_seg5_label, 5) }}
                         >
                             {claimsDetail.gl_seg5_label}
                         </List.Item>
@@ -359,9 +333,7 @@ class Index extends Component {
                     type="primary"
                     style={styles.button}
                     // onPressIn={() => this.props.navigation.navigate('EditSelfInfo', {status:'create'})}
-                    onClick={() => {
-                        this.onSubmit()
-                    }}
+                    onClick={()=>{this.onSubmit()}}
                     // onPress={()=>{alert(1)}}
                 >
                     确定
@@ -396,13 +368,13 @@ const styles = StyleSheet.create({
         borderRadius: 3,
         marginTop: 30,
         marginBottom: 10,
-        marginLeft: 15,
-        marginRight: 15,
+        marginLeft:15,
+        marginRight:15,
         backgroundColor: '#58cb8c',
         borderColor: 'transparent'
     },
     listLeftBorder: {
-        borderLeftWidth: 1 / PixelRatio.get(),
+        borderLeftWidth: 1/PixelRatio.get(),
         borderColor: '#e1e1e1',
         borderTopWidth: 0,
     },
